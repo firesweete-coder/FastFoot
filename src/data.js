948dc0f3a8cadcc15ebb14417950e198debf88ab
firesweete-coder <firesweete@gmail.com>
2026-04-22 00:35:22 +0100

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const TEAMS = [
+  { id: 'barcelona', name: 'برشلونة', logo: '🔵🔴', color: '#A50044', league: 'الإسباني', stadium: 'كامب نو', coach: 'تشافي', stars: ['ليفاندوفسكي', 'بيدري'], achievements: ['27 دوري', '5 أبطال أوروبا'] },
+  { id: 'real_madrid', name: 'ريال مدريد', logo: '⚪', color: '#FEBE10', league: 'الإسباني', stadium: 'برنابيو', coach: 'أنشيلوتي', stars: ['فينيسيوس', 'بيلينغهام'], achievements: ['35 دوري', '14 أبطال أوروبا'] },
+  { id: 'al_hilal', name: 'الهلال', logo: '🔵', color: '#0055A4', league: 'السعودي', stadium: 'الملك فهد', coach: 'جيسوس', stars: ['نيمار', 'مالكوم'], achievements: ['19 دوري', '4 أبطال آسيا'] },
+  { id: 'al_nassr', name: 'النصر', logo: '🟡', color: '#FFD700', league: 'السعودي', stadium: 'مرسول بارك', coach: 'كاسترو', stars: ['رونالدو', 'ماني'], achievements: ['9 دوري'] },
+  { id: 'raja', name: 'الرجاء', logo: '🟢', color: '#006400', league: 'المغربي', stadium: 'محمد الخامس', coach: 'زيمباور', stars: ['العروبي', 'زيلا'], achievements: ['13 بطولة', '3 أبطال أفريقيا'] },
+  { id: 'wydad', name: 'الوداد', logo: '🔴', color: '#DC143C', league: 'المغربي', stadium: 'محمد الخامس', coach: 'رمزي', stars: ['جبران', 'العملود'], achievements: ['22 بطولة', '3 أبطال أفريقيا'] },
+];
